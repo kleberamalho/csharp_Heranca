1faf7c1a760c375f99f5664f7b6a50d295668ec8
Kleber Ramalho <kleber.ramalho96@hotmail.com>
2022-09-14 18:12:16 -0300

    teste

---
+++ BangBank_ADM/BangBank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangBank_ADM.Funcionarios
+{
+    internal class Funcionario
+    {
+    }
+}
